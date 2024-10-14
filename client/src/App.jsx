d5106194909c636a89cb7bd7a2611e9e6b795005
Green00101 <2585640230@qq.com>
2024-10-14 11:32:15 +1100

    init commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button className="bg-red-600">Chgeck</Button>
+    </>
+  );
+}
+
+export default App;
