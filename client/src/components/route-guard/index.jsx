30b89bc8428ce3a8d7fdf75de70beb419b2dae5f
Green00101 <2585640230@qq.com>
2024-10-21 15:10:35 +1100

    work on route grad

---
+++ client/src/components/route-guard/index.jsx
@@ -0,0 +1,31 @@
+import { Fragment } from "react";
+
+const { useLocation, Navigate } = require("react-router-dom");
+
+function RouteGuard({ authenticated, user, element }) {
+  const location = useLocation();
+
+  if (!authenticated && !location.pathname.includes("/auth")) {
+    return <Navigate to={"/auth"} />;
+  }
+
+  if (
+    authenticated &&
+    user?.role !== "admin" &&
+    (location.pathname.includes("admin") || location.pathname.includes("/auth"))
+  ) {
+    return <Navigate to={"/home"} />;
+  }
+
+  if (
+    authenticated &&
+    user.role === "admin" &&
+    !location.pathname.includes("admin")
+  ) {
+    return <Navigate to={"/admin"} />;
+  }
+
+  return <Fragment>{element}</Fragment>;
+}
+
+export default RouteGuard;
---
+++ client/src/pages/instructor/index.jsx
@@ -0,0 +1,5 @@
+function InstructorDashboard() {
+  return <div></div>;
+}
+
+export default InstructorDashboard;
