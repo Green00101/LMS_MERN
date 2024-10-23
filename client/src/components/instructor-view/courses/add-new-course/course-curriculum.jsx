d7b3bcc636fb8ed6309122697f462c979fac4bd4
Green00101 <2585640230@qq.com>
2024-10-24 09:12:40 +1100

    add curriculum card

--- client/src/components/instructor-view/courses/add-new-course/course-curriculum.jsx
+++ client/src/components/instructor-view/courses/add-new-course/course-curriculum.jsx
@@ -1,5 +1,44 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Switch } from "@/components/ui/switch";
+import { InstructorContext } from "@/context/instructor-context";
+import { Label } from "@radix-ui/react-label";
+import { useContext } from "react";
+
 function CourseCurriculum() {
-  return <div>CourseCurriculum</div>;
+  const { courseCurriculumFormData, setCrouseCurriculumFormData } =
+    useContext(InstructorContext);
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Creat Course Curriculum</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Button>Add Lecture</Button>
+        <div className="mt-4 space-y-4">
+          {courseCurriculumFormData.map((curriculumItem, index) => (
+            <div className="border p-5 rounded-md">
+              <div className="flex gap-5 items-center">
+                <h3 className="font-semibold">Lecture {index + 1}</h3>
+                <Input
+                  name={`tittle-${index + 1}`}
+                  placeholder="Enter lecture tittle"
+                  className="max-w-96"
+                />
+                <div className="flex tiems-center space-x-2">
+                  <Switch checked={false} id={`freePreview-${index + 1}`} />
+                  <Label htmlFor={`freePreview-${index + 1}`}>
+                    Free Preview
+                  </Label>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </CardContent>
+    </Card>
+  );
 }
 
 export default CourseCurriculum;
