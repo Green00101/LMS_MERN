10e1bcf3944209ecfc1ea51600def6c45758af7f
Green00101 <2585640230@qq.com>
2024-10-24 09:41:15 +1100

    creat add lecture

--- client/src/components/instructor-view/courses/add-new-course/course-curriculum.jsx
+++ client/src/components/instructor-view/courses/add-new-course/course-curriculum.jsx
@@ -2,20 +2,32 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Switch } from "@/components/ui/switch";
+import { courseCurriculumInitialFormData } from "@/config";
 import { InstructorContext } from "@/context/instructor-context";
 import { Label } from "@radix-ui/react-label";
 import { useContext } from "react";
 
 function CourseCurriculum() {
-  const { courseCurriculumFormData, setCrouseCurriculumFormData } =
+  const { courseCurriculumFormData, setCourseCurriculumFormData } =
     useContext(InstructorContext);
+
+  function handleNewLecture() {
+    setCourseCurriculumFormData([
+      ...courseCurriculumFormData,
+      {
+        ...courseCurriculumInitialFormData[0],
+      },
+    ]);
+  }
+
+  console.log(courseCurriculumFormData);
   return (
     <Card>
       <CardHeader>
         <CardTitle>Creat Course Curriculum</CardTitle>
       </CardHeader>
       <CardContent>
-        <Button>Add Lecture</Button>
+        <Button onClick={handleNewLecture}>Add Lecture</Button>
         <div className="mt-4 space-y-4">
           {courseCurriculumFormData.map((curriculumItem, index) => (
             <div className="border p-5 rounded-md">
@@ -32,6 +44,9 @@ function CourseCurriculum() {
                     Free Preview
                   </Label>
                 </div>
+              </div>
+              <div className="mt-6">
+                <Input type="file" accept="video/*" className="mb-4" />
               </div>
             </div>
           ))}
