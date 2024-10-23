1316a7de2d1ac915c230b1b8bac2d3fb385e49cb
Green00101 <2585640230@qq.com>
2024-10-23 20:44:06 +1100

    add input in setting page

--- client/src/components/instructor-view/courses/add-new-course/course-setting.jsx
+++ client/src/components/instructor-view/courses/add-new-course/course-setting.jsx
@@ -1,5 +1,21 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 function CourseSetting() {
-  return <div>CourseSetting</div>;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Course Settings</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="flex flex-col gap-3">
+          <Label>Upload Course Image</Label>
+          <Input type="file" accept="image/*" />
+        </div>
+      </CardContent>
+    </Card>
+  );
 }
 
 export default CourseSetting;
