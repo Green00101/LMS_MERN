6b88b88888cfcfda8c7a1aacd94e04489a0b0c68
Green00101 <2585640230@qq.com>
2024-11-02 10:06:35 +1100

    creat basic layout

--- client/src/pages/student/course-details/index.jsx
+++ client/src/pages/student/course-details/index.jsx
@@ -1,8 +1,11 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
+import VideoPlayer from "@/components/video-player";
 import { StudentContext } from "@/context/student-context";
 import { fetchStudentViewCourseDetailsService } from "@/services";
+import { CheckCircle, Globe, Lock, PlayCircle } from "lucide-react";
 import { useContext, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 
 function StudentViewCourseDetailsPage() {
   const {
@@ -14,6 +17,7 @@ function StudentViewCourseDetailsPage() {
     setLoadingState,
   } = useContext(StudentContext);
   const { id } = useParams();
+  const location = useLocation();
 
   async function fetchStudentViewCourseDetails() {
     const response = await fetchStudentViewCourseDetailsService(
@@ -29,6 +33,10 @@ function StudentViewCourseDetailsPage() {
   }
 
   useEffect(() => {
+    if (!location.pathname.includes("course/details"))
+      setStudentViewCourseDetails(null), setCurrentCourseDetailsId(null);
+  }, [location.pathname]);
+  useEffect(() => {
     if (currentCourseDetailsId !== null) fetchStudentViewCourseDetails();
   }, [currentCourseDetailsId]);
   useEffect(() => {
@@ -36,7 +44,102 @@ function StudentViewCourseDetailsPage() {
   }, [id]);
 
   if (loadingState) return <Skeleton />;
-  return <div>StudentViewCourseDetailsPage</div>;
+
+  const getIndexOfFreePreviewUrl =
+    studentViewCourseDetails !== null
+      ? studentViewCourseDetails?.curriculum.findIndex(
+          (item) => item.freePreview
+        )
+      : -1;
+  return (
+    <div className="mx-auto p-4">
+      <div className="bg-gray-900 text-white p-8 rounded-t-lg">
+        <h1 className="text-3xl font-bold mb-4">
+          {studentViewCourseDetails?.title}
+        </h1>
+        <p className="text-xl mb-4">{studentViewCourseDetails?.subtitle}</p>
+        <div className="flex items-center space-x-4 mt-2 text-sm">
+          <span>Created By {studentViewCourseDetails?.instructorName}</span>
+          <span>Created On {studentViewCourseDetails?.date.split("T")[0]}</span>
+          <span className="flex items-center">
+            <Globe className="mr-1 h-4 w-4" />
+            {studentViewCourseDetails?.primaryLanguage}
+          </span>
+          <span>
+            {studentViewCourseDetails?.student.length}{" "}
+            {studentViewCourseDetails?.student.length <= 1
+              ? "Student"
+              : "Students"}
+          </span>
+        </div>
+      </div>
+      <div className="flex flex-col md:flex-row gap-8 mt-8">
+        <main className="flex-grow">
+          <Card className="mb-8">
+            <CardHeader>
+              <CardTitle>What you'll learn</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <ul className="grid grid-cols-1 md:grid-cols-2 gap-2">
+                {studentViewCourseDetails?.objectives
+                  .split(",")
+                  .map((objectives, index) => (
+                    <li key={index} className="flex items-start">
+                      <CheckCircle className="mr-2 w-5 text-green-500 flex-shrink-0" />
+                      <span>{objectives}</span>
+                    </li>
+                  ))}
+              </ul>
+            </CardContent>
+          </Card>
+          <Card className="mb-8">
+            <CardHeader>
+              <CardTitle>Course Curriculum</CardTitle>
+              <CardContent>
+                {studentViewCourseDetails?.curriculum?.map(
+                  (curriculumItem, index) => (
+                    <li
+                      className={`${
+                        curriculumItem?.freePreview
+                          ? "cursor-pointer"
+                          : "cursor-not-allowed"
+                      } flex items-center mb-4`}
+                    >
+                      {curriculumItem?.freePreview ? (
+                        <PlayCircle className="mr-2 h-4 w-4" />
+                      ) : (
+                        <Lock className="mr-2 h-4 w-4" />
+                      )}
+                      <span>{curriculumItem?.title}</span>
+                    </li>
+                  )
+                )}
+              </CardContent>
+            </CardHeader>
+          </Card>
+        </main>
+        <aside className="w-full md:w-[500px]">
+          <Card className="sticky top-4">
+            <CardContent className="p-6">
+              <div className="aspect-video mb-4 rounded-lg flex items-center justify-center">
+                <VideoPlayer
+                  url={
+                    getIndexOfFreePreviewUrl !== -1
+                      ? studentViewCourseDetails?.curriculum[
+                          getIndexOfFreePreviewUrl
+                        ].videoUrl
+                      : ""
+                  }
+                  width="450px"
+                  height="200px"
+                />
+              </div>
+            </CardContent>
+          </Card>
+        </aside>
+      </div>
+    </div>
+  );
 }
 
 export default StudentViewCourseDetailsPage;
